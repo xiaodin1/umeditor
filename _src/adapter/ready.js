383dc8af2ea03b81dac2b74b06a9bb4c9e295757
xuheng <pk3338455>
2013-07-23 16:07:32 +0800

    [fixed]byxuheng

--- _src/adapter/ready.js
+++ _src/adapter/ready.js
@@ -4,7 +4,7 @@
 UE.ready(function(){
     var T = this;
     this.addListener('click',function(){
-        T.$container.find('.dropdown-menu').each(function(){
+        T.$container.find('.edui-dropdown-menu').each(function(){
             $(this).edui().hide()
         })
     })
